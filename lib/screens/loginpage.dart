951449cc79725b7cab63b98c7ef809351954e2aa
chetal patel <chetalpatel14@gmail.com>
2022-02-23 17:58:28 +0530

    day4 complited

--- lib/screens/loginpage.dart
+++ lib/screens/loginpage.dart
@@ -1,11 +1,69 @@
 import 'package:flutter/material.dart';
-class login_page extends StatelessWidget {
+import 'package:google_fonts/google_fonts.dart';
+class login_page extends StatelessWidget
+ {
+  var ishover =true;
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("login page"),
+      
+      color: Colors.white,
+      child: Column(
+        children: [
+         SizedBox( height: 100, ),
+          Image.asset('image/login_image.png',fit:BoxFit.cover ,),
+          SizedBox( height: 30,),
+          Text('Login',style: TextStyle(color: Colors.blue,fontSize: 25,fontWeight: FontWeight.bold),),
+          SizedBox( height: 20,),
+         
+         Padding(padding:EdgeInsets.symmetric(vertical: 10,horizontal: 20), 
+          child: Column(
+             
+            children: [
+              TextFormField(
+                decoration:InputDecoration(
+                  hintText: "enter your name",
+                  labelText: "Name",
+                ),
+              ),
+              TextFormField(
+                 obscureText: true,
+                decoration:InputDecoration(
+                  hintText: "Password",
+                  labelText: "Password",
+                  
+                ),
+              ),
+            ],
+         )
+         ),
+         SizedBox(height: 40,),
+           
+          Row(
+           mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+               
+               onPressed:(){}, 
+               style:TextButton.styleFrom(padding:EdgeInsets.symmetric(horizontal: 35,)),
+               child:Text("Login"),
+               ),
+               SizedBox(
+                 width: 20,
+               ),
+                ElevatedButton(
+               
+               onPressed:(){}, 
+               style:TextButton.styleFrom(padding:EdgeInsets.symmetric(horizontal: 35,)),
+               child:Text("Cancel"),
+               ),
+            ],
+          ),
+        
+
+
+        ],
       ),
     );
   }
